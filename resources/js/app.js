ae9919a2bfffbba1d91cfcc574c24ca952aea698
unknown <jschueren1431@gmail.com>
2024-03-19 21:43:50 -0700

    completed vue setup and added the navbar to the home page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+
+import {createApp} from 'vue/dist/vue.esm-bundler.js';
+import Navbar from "./components/Navbar/Navbar.vue";
+import "../css/app.css";
+
+const app = createApp({
+    components: {
+        Navbar,
+    },
+});
+
+app.mount('#app');
---
+++ resources/js/components/Navbar/Navbar.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+<template>
+<div>
+    <div data-testid="navbar" className="nav-div">
+        <div className="nav-title">
+            <a href="/" className="nav-title">
+                <span data-testid="title" className="title-text">Taskify</span>
+                <img className="logo" alt="Company Logo"></img>
+            </a>
+            <div className="toggle-button">
+                <i className="fa-solid fa-bars fa-lg"></i>
+            </div>
+        </div>
+        <div className="link-div">
+            <a className="nav-link" href="/dashboard">About</a>
+            <a className="nav-link" href="/dashboard">Pricing</a>
+            <a className="nav-link" href="/dashboard">Showcase</a>
+            <a className="nav-link" href="/dashboard">Book an Appointment</a>
+        </div>
+        <div className="nav-auth">
+           <a className="nav-link" href="/login">Sign In</a>
+           <div className="highlight-div">
+               <a className="link-highlight nav-signin" href="/register">Sign Up</a>
+           </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<style scoped>
+    @import "./Navbar.css";
+</style>
